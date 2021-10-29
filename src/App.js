738a19307c1c656be7ee3932b92c60a558e5985c
mahmudul <mhasan.meet@gmail.com>
2021-10-30 00:31:03 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+//Import Components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+          <h2>Today's Listing</h2>
+      </header>
+
+      <Form/>
+      <TodoList/>
     </div>
   );
 }
